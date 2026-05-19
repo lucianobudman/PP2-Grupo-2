3a8e47ab9f41d4f1437e0eaab8dc8962cbdb5416
Pablo <pablo.cau15@gmail.com>
2026-05-19 17:41:10 -0300

    Clase 7 finalizada: agrego 1 bug

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -1,20 +1,20 @@
 //El Modelo es el "molde" de nuestra tabla. Reemplaza a las clases JS que hicimos en la Clase 3. 
-const { DataTypes } = require('sequelize'); 
+const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database'); // Importamos la conexión 
 // Definimos la tabla 'Productos' 
-const Producto = sequelize.define('Producto', { 
-// El ID se crea solo por defecto (Autoincremental), no hace falta ponerlo 
-nombre: { 
-type: DataTypes.STRING, 
-allowNull: false // Obligatorio 
-}, 
-precio: { 
-type: DataTypes.FLOAT, 
-defaultValue: 0 
-}, 
-stock: { 
-type: DataTypes.INTEGER, 
-defaultValue: 0 
-} 
-}); 
+const Producto = sequelize.define('Producto', {
+    // El ID se crea solo por defecto (Autoincremental), no hace falta ponerlo 
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false // Obligatorio 
+    },
+    precio: {
+        type: DataTypes.FLOAT,
+        defaultValue: 0
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
+});
 module.exports = Producto;
